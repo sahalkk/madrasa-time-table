6d42fb01fc19f362e9f2b04f9e0bd006b9ee9243
sahalkk <msahalkk357@gmail.com>
2023-09-11 22:24:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    console.log("data fetched");
+  }, []);
+
+  const [selectedValue, setSelectedValue] = useState("");
+
+  const handleSelectChange = (event) => {
+    const newValue = event.target.value;
+    setSelectedValue(newValue);
+    console.log(`Selected value: ${newValue}`);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="bg-blue-500 h-screen md:p-5 flex flex-col justify-center items-center">
+        <h1 className="text-2xl md:font-bold md:text-5xl">
+          MADRASA NAME, BANGLORE
+        </h1>
+        <h3 className="text-2xl md:font-bold md:text-3xl mb-10">TIME TABLE</h3>
+        <select
+          value={selectedValue}
+          onChange={handleSelectChange}
+          className="w-1/2 md:w-1/4 border border-gray-300 text-gray-700 py-3 px-4 pr-8 rounded focus:outline-none focus-border-gray-500"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <option value={""} disabled>
+            Select your class
+          </option>
+          <option value={"class1"}>class 1</option>
+          <option value={"class2"}>class 2</option>
+          <option value={"class3"}>class 3</option>
+        </select>
+      </div>
+    </>
   );
 }
 
